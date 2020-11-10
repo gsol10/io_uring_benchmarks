fdf0b831ed99d2f01e6726c43b5ac2e84fd6a784
GS <guillaume.solignac@polytechnique.edu>
2020-11-10 19:39:35 +0100

    Remove useless code

--- src/main.c
+++ src/main.c
@@ -25,29 +25,6 @@
 #define DEBUG_PRINT(fmt, args...)
 #endif
 
-//Returns the file descriptor of the socket
-int setup() {
-	int fd = socket(AF_INET, SOCK_DGRAM, 0);
-	
-	struct sockaddr_in laddr;
-
-	bzero(&laddr, sizeof(struct sockaddr_in));
-
-	laddr.sin_family    = AF_INET;
-    laddr.sin_addr.s_addr = INADDR_ANY;
-    laddr.sin_port = htons(10000);
-      
-    // Bind the socket with the server address
-	int r = 0;
-    if (r = bind(fd, (const struct sockaddr *)&laddr, sizeof(laddr)) < 0) {
-		char *error = strerror(r);
-        printf("Error binding socket : %s\n", error); 
-        return -1;
-    }
-
-	return fd;
-}
-
 int setup_packet(int ifindex) {
 	int fd = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_IP));
 	
@@ -88,81 +65,6 @@ struct msg_sent {
 	int ind;
 	int interface; //1 or 2 depending on the fd
 };
-
-int queue_sendmsg(int fd, struct io_uring *ring, struct msghdr *msg) {
-	int offset = 0;
-	int size = RECV_BUF_SIZE;
-	struct io_uring_sqe *sqe;
-
-	sqe = io_uring_get_sqe(ring);
-	if (!sqe) {
-		return 1;
-	}
-
-	io_uring_prep_sendmsg(sqe, fd, msg, 0);
-	io_uring_sqe_set_data(sqe, msg);
-	io_uring_submit(ring);
-	return 0;
-}
-
-static void queue_recvmsg(int fd, struct io_uring *ring, struct msghdr *msg) {
-	struct io_uring_sqe *sqe;
-	sqe = io_uring_get_sqe(ring);
-	DEBUG_PRINT("Sqe is %p\n", sqe);
-
-	io_uring_prep_recvmsg(sqe, fd, msg, 0);
-	io_uring_sqe_set_data(sqe, msg);
-	int n = io_uring_submit(ring);
-	DEBUG_PRINT("Sent %d sqes\n", n);
-}
-
-struct msghdr *alloc_msg() {
-	struct msghdr *msg = malloc(sizeof(struct msghdr));
-	struct sockaddr *s_addr = malloc(sizeof(struct sockaddr));
-	struct iovec *data = malloc(sizeof(struct iovec) + RECV_BUF_SIZE);
-
-	data->iov_base = data + 1;
-	data->iov_len = RECV_BUF_SIZE;
-
-	msg->msg_name = s_addr;
-	msg->msg_namelen = sizeof(s_addr);
-	msg->msg_iov = data;
-	msg->msg_iovlen = 1;
-
-	return msg;
-}
-
-int tests_io_uring(int fd) {
-	struct io_uring ring;
-	if (setup_context(64, &ring))
-		return 1;
-
-	struct msghdr *msg = alloc_msg();
-	queue_recvmsg(fd, &ring, msg);
-	//queue_read(fd, &ring);
-	while (1) {
-		struct io_uring_cqe *cqe;
-		int ret = io_uring_wait_cqe(&ring, &cqe);
-		if (ret == 0) {
-			DEBUG_PRINT("It worked !\n");
-			struct msghdr *msg = io_uring_cqe_get_data(cqe);
-			DEBUG_PRINT("Data is at %p, res is %d\n", msg, (cqe->res));
-
-			msg->msg_iov->iov_len = cqe->res;
-			
-			//data[10] = '\x00';
-			DEBUG_PRINT("Data = %s\n", (char *) msg->msg_iov->iov_base);
-
-			queue_sendmsg(fd, &ring, msg);
-			io_uring_wait_cqe(&ring, &cqe);
-			msg->msg_iov->iov_len = RECV_BUF_SIZE;
-			queue_recvmsg(fd, &ring, msg);
-		}
-	}
-
-	io_uring_queue_exit(&ring);
-	
-}
 
 int echo_io_uring(int fd1, int fd2) {
 	struct io_uring ring;
@@ -267,33 +169,6 @@ int main(int argc, char *argv[]) {
 
 	echo_io_uring(fd1, fd2);
 
-	// char buf[RECV_BUF_SIZE];
-	// int r = read(fd, buf, RECV_BUF_SIZE - 1);
-	// printf("Received : %d\n", r);
-	// for (int i = 0; i < 6; i++) {
-	// 	printf("%hhx", buf[i]);
-	// }
-
-	// int fd = setup();
-
-	// printf("Starting test\n");
-
-	// tests_io_uring(fd);
-
-	// char buf[RECV_BUF_SIZE];
-	// int r = recv(fd, buf, RECV_BUF_SIZE - 1, 0);
-	// DEBUG_PRINT("Received : %d\n", r);
-
-	// printf("Listening on socket. Receiving with vanilla recvfrom.\n");
-	// char buf[RECV_BUF_SIZE];
-	// struct sockaddr src_addr; socklen_t addrlen = sizeof(struct sockaddr);
-	// int n = 0;
-	// while (n = recvfrom(fd, buf, RECV_BUF_SIZE - 1, 0, &src_addr, &addrlen)) {
-	// 	buf[n] = '\x00';
-	// 	printf("Received message : %s\n", buf);
-
-	// 	addrlen = sizeof(struct sockaddr);
-	// }
 	close(fd1);
 	close(fd2);
 
