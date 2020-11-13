e512c54644cb541e8ea873bfe5c63e60fc3b3db0
GS <guillaume.solignac@polytechnique.edu>
2020-11-13 19:09:21 +0100

    Good start for proper poll code

--- src/main.c
+++ src/main.c
@@ -12,6 +12,8 @@
 #include <net/ethernet.h>
 #include <linux/if_packet.h>
 
+ #include <poll.h>
+
 #include <string.h>
 #include <strings.h>
 #include <unistd.h>
@@ -60,15 +62,23 @@ static int setup_context(unsigned entries, struct io_uring *ring)
 	return 0;
 }
 
+enum {
+	EVENT_POLL,
+	EVENT_READ,
+	EVENT_WRITE
+};
+
 struct msg_sent {
-	int read;
+	int op_type;
 	int ind;
 	int interface; //1 or 2 depending on the fd
 };
 
 int echo_io_uring(int fd1, int fd2) {
 	struct io_uring ring;
-	int req_size = 1024;
+	int req_size = 8;
+
+	int fds[2] = {fd1, fd2};
 
 	if (setup_context(req_size, &ring))
 		return 1;
@@ -83,14 +93,26 @@ int echo_io_uring(int fd1, int fd2) {
 	struct io_uring_sqe *sqe;
 
 	//We fill the half of the sqes with read requests for both interfaces
-	for (int i = 0; i < req_size/2; i++) {
-		sqe = io_uring_get_sqe(&ring);
+	for (int32_t i = 0; i < 4; i+=2) {
+		int interface = (i % 4)/2;
+		int fd = fds[interface];
+
 		int32_t ind = i;
-		int interface = (i % 2) + 1;
-		int fd = interface == 1 ? fd1 : fd2;
+		sqe = io_uring_get_sqe(&ring);
+		io_uring_prep_poll_add(sqe, fd, POLLIN);
+		sqe->flags |= IOSQE_IO_LINK; //Link so that next submission is executed just after this one
+		info[ind].ind = ind;
+		info[ind].op_type = EVENT_POLL;
+		info[ind].interface = interface;
+		io_uring_sqe_set_data(sqe, &info[ind]);
+
+
+		ind = i + 1;
+		sqe = io_uring_get_sqe(&ring);
+		sqe->flags = 0;
 		io_uring_prep_readv(sqe, fd, &iov[ind], 1, 0);
 		info[ind].ind = ind;
-		info[ind].read = 1;
+		info[ind].op_type = EVENT_READ;
 		info[ind].interface = interface;
 		io_uring_sqe_set_data(sqe, &info[ind]);
 	}
@@ -106,27 +128,63 @@ int echo_io_uring(int fd1, int fd2) {
 		DEBUG_PRINT("CQE read is %d\n", ret);
 		for (int i = 0; i < nb_req; i++) {
 			struct io_uring_cqe *cqe = cqes[i];
-			DEBUG_PRINT("It worked !\n");
 			struct msg_sent *inf = (struct msg_sent *) io_uring_cqe_get_data(cqe);
-			DEBUG_PRINT("Buffer index is %d, read is %d, res is %d\n", inf->ind, inf->read, (cqe->res));
-			int32_t ind = inf->ind;
-			int fd = inf->interface == 1 ? fd2 : fd1; //Send on the other interface
-			inf->interface = 3 - inf->interface;
-			if (inf->read == 1) {
+			int32_t interface = inf->interface;
+			int ind = inf->ind;
+			int type = inf->op_type;
+			int fd = fds[1 - interface]; //Opposite fd
+			int ret = cqe->res;
+
+			io_uring_cqe_seen(&ring, cqe);
+
+			DEBUG_PRINT("Buffer index is %d, op_type is %d, res is %d\n", ind, type, ret);
+			switch (type)
+			{
+			case EVENT_POLL:
+				break;
+			case EVENT_READ:
+				ind -= 1; //We know the poll was already seen so the previous info is free
 				sqe = io_uring_get_sqe(&ring);
-				iov[ind].iov_len = cqe->res;
+				io_uring_prep_poll_add(sqe, fd, POLLOUT); //Check when fd is ready to write
+				sqe->flags |= IOSQE_IO_LINK; //Link so that next submission is executed just after this one
+				info[ind].ind = ind;
+				info[ind].op_type = EVENT_POLL;
+				info[ind].interface = interface;
+				io_uring_sqe_set_data(sqe, &info[ind]);
+
+				ind+=1;
+				sqe = io_uring_get_sqe(&ring);
+				sqe->flags = 0;
+				iov[ind].iov_len = ret;
 				io_uring_prep_writev(sqe, fd, &iov[ind], 1, 0);
-				inf->read = 0;
+				info[ind].ind = ind;
+				info[ind].op_type = EVENT_WRITE;
+				info[ind].interface = interface;
 				io_uring_sqe_set_data(sqe, &info[ind]);
-			} else {
+
+				break;			
+			case EVENT_WRITE:
+				ind -= 1; //We know the poll was already seen so the previous info is free
 				sqe = io_uring_get_sqe(&ring);
-				iov[ind].iov_len = RECV_BUF_SIZE; //We read max len again.
-				io_uring_prep_readv(sqe, fd, &iov[ind], 1, 0);
-				inf->read = 1;
+				io_uring_prep_poll_add(sqe, fd, POLLIN); //Check when fd is ready to read
+				sqe->flags |= IOSQE_IO_LINK; //Link so that next submission is executed just after this one
+				info[ind].ind = ind;
+				info[ind].op_type = EVENT_POLL;
+				info[ind].interface = interface;
 				io_uring_sqe_set_data(sqe, &info[ind]);
+
+				ind+=1;
+				sqe = io_uring_get_sqe(&ring);
+				sqe->flags = 0;
+				iov[ind].iov_len = RECV_BUF_SIZE;
+				io_uring_prep_writev(sqe, fd, &iov[ind], 1, 0);
+				info[ind].ind = ind;
+				info[ind].op_type = EVENT_READ;
+				info[ind].interface = interface;
+				io_uring_sqe_set_data(sqe, &info[ind]);
+				break;
 			}
 		}
-		io_uring_cq_advance(&ring, nb_req); //Equivalent to seen
 		io_uring_submit(&ring);
 	}
 	free(cqes);
